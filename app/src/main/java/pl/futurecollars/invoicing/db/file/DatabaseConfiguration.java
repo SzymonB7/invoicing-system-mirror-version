91994b90c55b68cf6b09bd0e7f92bad118ac606f
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-04-11 20:09:42 +0200

    Spring profiles added

--- app/src/main/java/pl/futurecollars/invoicing/db/file/DatabaseConfiguration.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/file/DatabaseConfiguration.java
@@ -3,9 +3,12 @@ package pl.futurecollars.invoicing.db.file;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import pl.futurecollars.invoicing.db.Database;
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -19,9 +22,16 @@ public class DatabaseConfiguration {
     return new IdService(idFilePath, fileService);
   }
 
+  @ConditionalOnProperty (name = "invoicing-system.database", havingValue = "file")
   @Bean
   public Database fileBasedDatabase(IdService idService, FileService fileservice, JsonService jsonService) throws IOException {
     Path databaseFilePath = Files.createTempFile(DATABASE_LOCATION, INVOICES_FILE_NAME);
     return new FileBasedDatabase(fileservice, jsonService, idService, databaseFilePath);
+  }
+
+  @ConditionalOnProperty (name = "invoicing-system.database", havingValue = "memory")
+  @Bean
+  public Database inMemoryDatabase() {
+    return new InMemoryDatabase();
   }
 }
