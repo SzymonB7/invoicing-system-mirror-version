9001504be62e29ab09dcb4bdc3102783770a9f1b
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-04-23 21:00:25 +0200

    Tax Calculator and Tax Bill created

--- app/src/main/java/pl/futurecollars/invoicing/db/Database.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/Database.java
@@ -1,8 +1,13 @@
 package pl.futurecollars.invoicing.db;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import lombok.Builder;
 import pl.futurecollars.invoicing.model.Invoice;
+import pl.futurecollars.invoicing.model.InvoiceEntry;
 
 public interface Database {
   Integer save(Invoice invoice);
@@ -14,5 +19,13 @@ public interface Database {
   void update(Integer id, Invoice updatedInvoice);
 
   void delete(Integer id);
+
+  default BigDecimal visit(Predicate<Invoice> invoicePredicate,
+                           Function<InvoiceEntry, BigDecimal> invoiceEntryToAmount) {
+    return getAll().stream()
+        .flatMap(invoice -> invoice.getInvoiceEntries().stream())
+        .map(invoiceEntryToAmount)
+        .reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
 
 }
